3f1d85b1c86c3374a6073707d270235ff5fb374c
Tyuiu.TretyakovDV@outlook.com <trev1793@gmail.com>
2023-10-01 14:40:47 +0500

    Add project files.

---
+++ Tyuiu.TretyakovDV.Sprint1.Task0.V4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.TretyakovDV.Sprint1.Task0.V4.Lib;
+namespace Tyuiu.TretyakovDV.Sprint1.Task0.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Третьяков Д.В. | ПКТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Третьяков Денис Викторович | ПКТб-23-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение 4/2*5/(3+2)*(5-2) и     *");
+            Console.WriteLine("* печатает результат на экране.                                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 4/2*5/(3+2)*(5-2)                                                        ");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
